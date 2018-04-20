3c24dcd10f8a960a65998061dcbad446e0f763d0
Lucy Davies <lucy.davies@skybettingandgaming.com>
2018-04-20 23:52:47 +0100

    Spilling to adjacent cells

--- matrix/main.go
+++ matrix/main.go
@@ -1,9 +1,5 @@
 package main
 
-// TODO: use a uint8
-// That way, we know we have a max of 255 (which is more than enough for this, I reckon)
-// And we don't need to calculate cell width on the fly
-
 import (
 	"fmt"
 	"math"
@@ -22,17 +18,26 @@ const (
 	CellBuffer = 1
 
 	// How many of the cells will increment every tick
-	ChanceOfIncrement = 0.01 // TODO: this should be a variable, inversely proportional to the sum of the matrix
+	// TODO: this should be a variable, inversely proportional to the sum of the matrix
+	ChanceOfIncrement = 0.0005
+
+	// How much of a cell's value should spill out to neighbours every tick
+	// This is in relation to half of the difference between the cell's value and the neighbour's value
+	SpillRate = 1
+
+	// TODO: variable, how frequently per second should something update
+	// or, how many things should update per second
+	// Use this to calcualte UpdatesPerSecond
 
 	// How much will it increment by
-	IncrementAmount = 1
+	IncrementAmount = 255
 
 	// How many ticks to attempt every second
 	// This can be super high, but would then essentially fill up immediatelly
-	UpdatesPerSecond = 600
+	TicksPerSecond = 48 // TODO: allow this to be < 1
 
 	// How often to refresh the view
-	FramesPerSecond = 30
+	FramesPerSecond = 24
 )
 
 var (
@@ -40,11 +45,11 @@ var (
 	MaxDisplay = int(math.Pow(16, CellWidth))
 	FullString = strings.Repeat("X", CellWidth)
 
-	matrix [][]int
+	matrix [][]float32
 )
 
 // printMatrix just prints it to screen
-func printMatrix(matrix [][]int) {
+func printMatrix(matrix [][]float32) {
 
 	// print the matrix
 
@@ -56,10 +61,10 @@ func printMatrix(matrix [][]int) {
 			cellString := ""
 
 			// if bigger than we can display
-			if cellValue >= MaxDisplay {
+			if int(cellValue) >= MaxDisplay {
 				cellString = fmt.Sprintf(" %s", FullString)
 			} else {
-				cellString = fmt.Sprintf(" %*x", CellWidth, cellValue)
+				cellString = fmt.Sprintf(" %*x", CellWidth, int(cellValue))
 			}
 
 			fmt.Printf(cellString)
@@ -69,7 +74,7 @@ func printMatrix(matrix [][]int) {
 }
 
 // randomIncrementMatrix will, for every cell, with a certain percentage, either increment or not
-func randomIncrementMatrix(matrix [][]int) {
+func randomIncrementMatrix(matrix [][]float32) {
 
 	// Each row has the same number of cols, so use first col
 	numRows := len(matrix)
@@ -78,6 +83,11 @@ func randomIncrementMatrix(matrix [][]int) {
 
 	cellsToIncrement := int(float32(numCells) * ChanceOfIncrement)
 	// TODO: what happens if this is zero?
+	if cellsToIncrement == 0 {
+		cellsToIncrement = 1
+		// For now, this will do
+		// In this case, we need to use this to determine if ANY cells increment
+	}
 
 	for i := 0; i < cellsToIncrement; i++ {
 		randCellRow := rand.Intn(numRows)
@@ -88,18 +98,164 @@ func randomIncrementMatrix(matrix [][]int) {
 
 }
 
-// stuffHappens is the loop where stuff actually happens
-func stuffHappens() {
+// spillToNeighbours will take some of a cell's value and spill it out to neighbours
+func spillToNeighbours(matrix [][]float32) {
+
+	// Each row has the same number of cols, so use first col
+	numRows := len(matrix)
+	numCols := len(matrix[1])
+
+	// Dupe the matrix, to store spill values
+	spillMatrix := make([][]float32, numRows)
+	for i := range spillMatrix {
+		spillMatrix[i] = make([]float32, numCols)
+	}
+
+	// TODO: keep track of non-empty cells
+
+	for row := range matrix {
+		for col := range matrix[row] {
+			cellValue := matrix[row][col]
+
+			//
+			// First, check if we can spill anywhere
+			//
+
+			// Assume we cannot spill anywhere
+			var spillDirections uint8 = 0
+			var spillEastAmount float32 = 0
+			var spillWestAmount float32 = 0
+			var spillNorthAmount float32 = 0
+			var spillSouthAmount float32 = 0
+
+			// Check north
+			if row > 0 {
+				cellValueNorth := matrix[row-1][col]
+
+				// If the value is lower than ours...
+				if cellValueNorth < cellValue {
+					spillDirections++
+
+					diffNorth := cellValue - cellValueNorth
+
+					// Spill at most half the diff
+					spillNorthAmount = diffNorth * SpillRate / 2
+				}
+			}
+
+			// Check south
+			if row < numRows-1 {
+				cellValueSouth := matrix[row+1][col]
+
+				// If the value is lower than ours...
+				if cellValueSouth < cellValue {
+					spillDirections++
+
+					diffSouth := cellValue - cellValueSouth
+
+					// Spill at most half the diff
+					spillSouthAmount = diffSouth * SpillRate / 2
+				}
+			}
+
+			// Check east
+			if col < numCols-1 {
+				cellValueEast := matrix[row][col+1]
+
+				// If the value is lower than ours...
+				if cellValueEast < cellValue {
+					spillDirections++
+
+					diffEast := cellValue - cellValueEast
+
+					// Spill at most half the diff
+					spillEastAmount = diffEast * SpillRate / 2
+				}
+			}
+
+			// Check west
+			if col > 0 {
+				cellValueWest := matrix[row][col-1]
+
+				// If the value is lower than ours...
+				if cellValueWest < cellValue {
+					spillDirections++
+
+					diffWest := cellValue - cellValueWest
+
+					// Spill at most half the diff
+					spillWestAmount = diffWest * SpillRate / 2
+				}
+			}
+
+			//
+			// keep track of spill amounts in a separate matrix
+			// otherwise there will be a kind of prevailing wind, in the south easterly direction
+			//
+
+			if spillNorthAmount > 0 {
+				// Spill North
+				spillNorthAmount = spillNorthAmount / float32(spillDirections)
+				spillMatrix[row][col] = spillMatrix[row][col] - spillNorthAmount
+				spillMatrix[row-1][col] = spillMatrix[row-1][col] + spillNorthAmount
+			}
+
+			if spillSouthAmount > 0 {
+				// Spill South
+				spillSouthAmount = spillSouthAmount / float32(spillDirections)
+				spillMatrix[row][col] = spillMatrix[row][col] - spillSouthAmount
+				spillMatrix[row+1][col] = spillMatrix[row+1][col] + spillSouthAmount
+			}
+
+			if spillEastAmount > 0 {
+				// Spill East
+				spillEastAmount = spillEastAmount / float32(spillDirections)
+				spillMatrix[row][col] = spillMatrix[row][col] - spillEastAmount
+				spillMatrix[row][col+1] = spillMatrix[row][col+1] + spillEastAmount
+			}
+
+			if spillWestAmount > 0 {
+				// Spill West
+				spillWestAmount = spillWestAmount / float32(spillDirections)
+				spillMatrix[row][col] = spillMatrix[row][col] - spillWestAmount
+				spillMatrix[row][col-1] = spillMatrix[row][col-1] + spillWestAmount
+			}
+
+		}
+	}
+
+	//
+	// Spill!
+	//
+
+	for row := range matrix {
+		for col := range matrix[row] {
+			cellValue := matrix[row][col]
+			spillValue := spillMatrix[row][col]
+
+			matrix[row][col] = cellValue + spillValue
+		}
+	}
+
+}
+
+// tick is the loop where stuff actually happens
+func tick() {
 
 	for {
-		// TODO
 		// Distribute some of your value to neighbours
-		// Requires keeping track of non-empty cells
+		spillToNeighbours(matrix)
 
 		// Random Increment
 		randomIncrementMatrix(matrix)
 
-		time.Sleep(time.Second / UpdatesPerSecond)
+		if TicksPerSecond < 1 {
+			SecondsPerTick := 1 / TicksPerSecond
+
+			time.Sleep(time.Second * time.Duration(SecondsPerTick))
+		} else {
+			time.Sleep(time.Second / TicksPerSecond)
+		}
 	}
 }
 
@@ -112,9 +268,9 @@ func init() {
 	cols := tw / (CellWidth + CellBuffer)
 
 	// Create a matrix of integers, same dimensions as the terminal
-	matrix = make([][]int, rows)
+	matrix = make([][]float32, rows)
 	for i := range matrix {
-		matrix[i] = make([]int, cols)
+		matrix[i] = make([]float32, cols)
 	}
 
 	// Seed the RNG
@@ -127,7 +283,7 @@ func main() {
 	fmt.Print("\033[?25l")       // hide cursor
 	defer fmt.Print("\033[?25h") //unhide cursor
 
-	go stuffHappens()
+	go tick()
 
 	for {
 		// By moving cursor to top-left position we ensure that console output
